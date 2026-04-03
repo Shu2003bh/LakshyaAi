cd614210ddd98b4b3ca9822f2a9a09e7e893ab7e
shu2003bh <shubhambajpai592@gmail.com>
2026-04-03 19:15:55 +0530

    Dashboard Updated

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -18,28 +18,23 @@
 //     </main>
 //   );
 // 
-import { getIndustryInsights } from "@/actions/industry-insight";
-import { getDashboardData } from "@/actions/dashboard";
-import DashboardView from "../dashboard/_component/dashboard-view";
+// 
+
 import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
+import OnboardingForm from "./_components/onboarding-form";
 import { getUserOnboardingStatus } from "@/actions/onboarding";
 
-export default async function DashboardPage() {
+export default async function OnboardingPage() {
   const { isOnboarded } = await getUserOnboardingStatus();
 
-  if (!isOnboarded) {
-    redirect("/onboarding");
+  if (isOnboarded) {
+    redirect("/dashboard");
   }
 
-  // fetch both in parallel
-  const [insights, dashboardData] = await Promise.all([
-    getIndustryInsights(),
-    getDashboardData(),
-  ]);
-
   return (
-    <div className="container mx-auto">
-      <DashboardView insights={insights} dashboardData={dashboardData} />
-    </div>
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
   );
 }
